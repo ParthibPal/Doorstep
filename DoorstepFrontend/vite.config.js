b540344dd30a21d93cae4ae6524bebcc7697bdeb
Parthib <palparthib97@gmail.com>
2025-09-17 00:18:16 +0530

    path

--- DoorstepFrontend/vite.config.js
+++ DoorstepFrontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Doorstep/',
+  base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000' 
