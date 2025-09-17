d2242469b1b5f3e37de8f3a68ff62809b36a3c80
Parthib <palparthib97@gmail.com>
2025-09-17 08:14:37 +0530

    Import Changes

--- DoorstepFrontend/src/Components/ActualComponent/Footer.jsx
+++ DoorstepFrontend/src/Components/ActualComponent/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ActualComponent_css/Footer.css'
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
     // const navigate = useNavigate();
     return (
