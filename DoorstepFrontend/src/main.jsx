fe8d8b8fcf61263797ceb31c81bb3f0d4d959ee6
Parthib <palparthib97@gmail.com>
2025-09-17 08:24:13 +0530

    Import Changes

--- DoorstepFrontend/src/main.jsx
+++ DoorstepFrontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import LogIn from './Components/LogIn.jsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
+import App from './App.jsx';
+import './index.css';
 import Context from './Components/CartOperations/Context.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Context>
-    <App />
-    </Context>
+    <BrowserRouter> {/* ✅ Wrap your app in the router */}
+      <Context>
+        <App />
+      </Context>
+    </BrowserRouter>
   </StrictMode>
-)
+);
